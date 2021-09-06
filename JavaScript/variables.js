2ffa5e7fec009b87024b40a7dffed96c09c943cd
danymessi1000@gmail.com <danymessi1000@gmail.com>
2021-09-06 17:27:44 -0500

    javascript tabla

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -1,4 +1,30 @@
-let nombre="Daniel"
 
 
-document.write(nombre)
+//Definicion de Variables
+//Constantes y variables
+// const pi=3.1416
+//let nombre="daniel"
+//let nombre=prompt("Ingrese su nombre")
+//document.write(nombre)
+
+
+//alert("a")
+
+//Concatenacion
+
+//let nombre=prompt("Ingrese su nombre:")
+//const frase=" ¿COMO ESTAS?"
+
+//frasefinal=nombre+frase
+
+//document.write(frasefinal)
+
+//let numero1=10;
+//let numero2=20;
+
+//concatenacion=""+numero1+numero2
+
+//suma=numero1+numero2
+
+//document.write(concatenacion)
+//document.write(suma)
