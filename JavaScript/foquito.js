61ee357da5c8aa2cb6e392856d7ae48442e0f06c
danymessi1000@gmail.com <danymessi1000@gmail.com>
2021-09-19 16:58:40 -0500

    foquito

--- JavaScript/foquito.js
+++ JavaScript/foquito.js
@@ -2,13 +2,17 @@ const contenedor=document.querySelector(".contenedor");
 const documentFragment=document.createDocumentFragment();
 
 
+
 let img0="<img id='myImage' src='../static/img/foquito0.jpeg'></img>"
-let img1="<img id='myImage' src='../static/img/foquito1.jpeg'></img>"
+
 let div=document.createElement("DIV");
 div.classList.add("Foquito")
+div.innerHTML=(img0)
+documentFragment.appendChild(div);
+contenedor.appendChild(documentFragment);
 
-function Foquito0(img0){
-   
+function Foquito0(){
+    let img0="<img id='myImage' src='../static/img/foquito0.jpeg'></img>"
     
     
     div.innerHTML=(img0);
@@ -17,8 +21,8 @@ function Foquito0(img0){
     contenedor.appendChild(documentFragment);
 }
 
-function Foquito1(img1){
-   
+function Foquito1(){
+    let img1="<img id='myImage' src='../static/img/foquito1.jpeg'></img>"
 
     div.innerHTML=(img1);
     
